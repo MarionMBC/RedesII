61605b9762be71193bf452f22ee3e245a87628c5
MarionMBC <marion.bustamante@unah.hn>
2022-08-16 00:15:52 -0600

    Sweet

--- js/controlador.js
+++ js/controlador.js
@@ -2,4 +2,22 @@ var scene = document.getElementById('scene');
 console.log(scene);
 var parallaxInstance = new Parallax(scene);
 console.log(parallaxInstance);
-console.log("hola")
+
+function sweer () {
+    Swal.isVisible()
+    
+}
+
+Swal.fire({
+    title: 'Custom width, padding, color, background.',
+    width: 600,
+    padding: '3em',
+    color: '#716add',
+    background: '#fff url(/images/trees.png)',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/nyan-cat.gif")
+      left top
+      no-repeat
+    `
+  })
