e00183ea96500a09802fb1979c7e36525d28ac71
MarionMBC <marion.bustamante@unah.hn>
2022-08-16 00:48:40 -0600

    Página finalizada

--- js/controlador.js
+++ js/controlador.js
@@ -3,21 +3,25 @@ console.log(scene);
 var parallaxInstance = new Parallax(scene);
 console.log(parallaxInstance);
 
-function sweer () {
-    Swal.isVisible()
+document.getElementById('btnAlert').addEventListener('click', function(){
+    Swal.fire({
+        title: 'Gracias por la mejor clase, Ing. Alex Moncada',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff url(https://c.tenor.com/Rdz9M0h2BoQAAAAC/confetti.gif)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("https://i.pinimg.com/originals/e5/93/ab/e593ab0589d5f1b389e4dfbcce2bce20.gif")
+          left top
+          no-repeat
+        `,
+        confirmButtonText: 'Terminar Periodo',
+      })
+});
     
-}
+        
+    
+            
+    
 
-Swal.fire({
-    title: 'Custom width, padding, color, background.',
-    width: 600,
-    padding: '3em',
-    color: '#716add',
-    background: '#fff url(/images/trees.png)',
-    backdrop: `
-      rgba(0,0,123,0.4)
-      url("/images/nyan-cat.gif")
-      left top
-      no-repeat
-    `
-  })
